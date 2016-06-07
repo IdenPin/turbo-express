639b3ada1a0766498bfcd87328a618d316724f3e
pdeng <pdeng@thinkjoy.cn>
2016-06-08 00:51:30 +0800

    add ZGK some static file

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var hbs = require('hbs');  //引入hbs
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -61,6 +60,25 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+
+//自定义helper
+var blocks = {};
+hbs.registerHelper('extend', function(name, context) {
+  var block = blocks[name];
+  if (!block) {
+    block = blocks[name] = [];
+  }
+  block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+  var val = (blocks[name] || []).join('\n');
+  // clear the block
+  blocks[name] = [];
+  return val;
+});
+
 
 
 module.exports = app;
